cc778a590bd71bf4a986d7cc836e910f48f35849
Pheonix999777 <Pheonix4777@gmail.com>
2024-08-19 10:53:46 +0500

    dsadasdsads

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
+import { Achievements } from "./Achievements/Achievements";
+import { Brands } from "./Brands/Brands";
+import { Faqs } from "./Faqs/Faqs";
+import { Hero } from "./Hero/Hero";
+import { Product } from "./Product/Product";
+
 export default function Home() {
   return (
     <>
-      {/* <Hero />
+      <Hero />
       <Brands />
       <Product />
       <Faqs />
-      <Achievements /> */}
+      <Achievements />
     </>
   );
 }
